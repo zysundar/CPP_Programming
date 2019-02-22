16871c33cb66165fecbf4fddc3d1b175239636b0
SUNDARAM MISHRA <zx.sundaram@gmail.com>
2019-02-22 10:46:54 +0530

    Update and rename Structer_students_marks.cpp to Structer_students_marks_calculation.cpp

--- Structer_students_marks_calculation.cpp
+++ Structer_students_marks_calculation.cpp
@@ -2,18 +2,22 @@
 #include<conio.h>
 using namespace std;
 struct student
-{int m1;
-int m2;
-int m3;
+{
+int sub1_marks;
+int sub2_marks;
+int sub3_marks;
 char name[10];
 };
+
 int main()
-{struct student s[10];
+{
+struct student s[10];
 int n;
-cout<<"how many student want to calculate total marks";
+cout<<"How many student's marks you want to calculate";
 cin>>n;
 for(int i=0;i<n;i++)
-{cout<<"enter the marks and name"<<endl;
+{
+cout<<"enter the marks of student"<<i+1<<" and name"<<endl;
 cin>>s[i].m1;
 cin>>s[i].m2;
 cin>>s[i].m3;
@@ -22,5 +26,5 @@ int totalmarks=(s[i].m1+s[i].m2+s[i].m3);
 cout<<"total marks="<<totalmarks;
 cout<<"name="<<s[i].name[10]<<endl;
 }
-getch();
+
 }
