b8c1726a2ce07ae94e7b1cdd2d0676517314b354
SUNDARAM MISHRA <zx.sundaram@gmail.com>
2019-02-25 00:52:42 +0530

    Update and rename data conversion.cpp to Different distance conversion using constructer.cpp

--- Different distance conversion using constructer.cpp
+++ Different distance conversion using constructer.cpp
@@ -1,32 +1,40 @@
 #include<conio.h>
 #include<iostream>
 using namespace std;
-class distance
-{private:
+
+class Distance
+{
+private:
 	float mtf;
 	int feet;
 	float inches;
-	public:
-		distance()
-		{feet=0;
+public:
+	Distance()
+		{
+		feet=0;
 		inches=0;
 		mtf=3.2890;
 		}
-		distance(float m)
-		{ float meter=m;
+	Distance(float m)
+		{ 
+		float meter=m;
 		float fltfeet=meter*mtf;
 		feet=int(fltfeet);
 		inches=12*feet;
-		cout<<fltfeet;
+		//cout<<fltfeet;
 		}
-	/*	void show()
-		{cout<<feet;
+	void show()
+		{
+		cout<<feet;
 		cout<<inches;
 		}
-*/
+
 };
+
 int main()
-{class distance d(6.00);
-//d.show();
-getch();
+{
+class distance d(6.00);
+d.show();
+
+return 0;
 }
