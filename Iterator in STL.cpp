ad7480c5f9f38a254772ca2fda775a4e30a24151
SUNDARAM MISHRA <zx.sundaram@gmail.com>
2019-02-25 01:19:37 +0530

    Update and rename iterator.cpp to Iterator in STL.cpp

--- Iterator in STL.cpp
+++ Iterator in STL.cpp
@@ -3,16 +3,18 @@
 #include<vector>
 using namespace std;
 main()
-{int arr[]={1,2,34,45};
+{i
+ nt arr[]={1,2,34,45};
 vector<int>v(arr,arr+4);
 for(vector<int>::iterator i=v.begin();i!=v.end();i++)
-{cout<<*i<<endl;
-
+{
+  cout<<*i<<endl;
 }
 v.push_back(8);
 while(!v.empty())
-{cout<<v.back()<<"  ";
-v.pop_back();
+{
+  cout<<v.back()<<"  ";
+  v.pop_back();
 }
 getch();
 }
