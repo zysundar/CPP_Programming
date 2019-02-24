f3d55c2f53bfac41d7bedbdb5343798f9bcf071f
SUNDARAM MISHRA <zx.sundaram@gmail.com>
2019-02-25 01:08:23 +0530

    Update and rename fail file.cpp to Dealing two file simultaneously.cpp

--- Dealing two file simultaneously.cpp
+++ Dealing two file simultaneously.cpp
@@ -4,23 +4,32 @@
 #include<string>
 #include<stdlib.h>
 using namespace std;
+
 int main()
-{ifstream in_stream;
+{
+ifstream in_stream;
 ofstream out_stream;
 int num;
-in_stream.open("sundaram.cpp");
-if(in_stream.fail())
-{cout<<"input file could not be opened";
-exit(1);
-}
+
+  in_stream.open("sundaram.cpp");
+
+  if(in_stream.fail())
+  {
+    cout<<"input file could not be opened";
+    exit(1);
+  }
+  
 out_stream.open("squares.dat");
-if(out_stream.fail())
-{cout<<"output file could not open";
-exit(1);
-}
+
+  if(out_stream.fail())
+  {
+      cout<<"output file could not open";
+      exit(1);
+  }
 in_stream>>num;
 out_stream<<"the square of"<<num<<"is"<<num*num;
 in_stream.close();
 out_stream.close();
-getch();
+
+ return 0;
 }
