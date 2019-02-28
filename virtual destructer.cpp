ff503ff6a7bcdb9e066d1359fb02246261068fac
SUNDARAM MISHRA <zx.sundaram@gmail.com>
2019-02-28 13:34:19 +0530

    Update and rename lok.cpp to virtual destructer.cpp

--- virtual destructer.cpp
+++ virtual destructer.cpp
@@ -1,12 +1,14 @@
 #include<conio.h>
 #include<iostream>
 using namespace std;
+
 class Base
 {
  public:
  virtual ~Base() {cout << "Base Destructor\t"; }
  Base(){cout<<"base constructrer";}
 };
+
 class Derived:public Base
 {
  public:
